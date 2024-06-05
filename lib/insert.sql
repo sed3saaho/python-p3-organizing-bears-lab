c0e044c116c4abb91b5763616d0694b73866ff80
Sedly <sedly.keith@student.moringaschool.com>
2024-06-05 11:25:06 +0300

    python-p3-organizing-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 10, "M", "Black", "aggressive", 2);
+INSERT INTO bears VALUES (2,"Rowdy", 20, "M", "blue", "slimy", 3);
+INSERT INTO bears VALUES (3,"Tabitha", 20, "F", "dark brown", "goofy", 4);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 7, "M", "Green", "intense", 2);
+INSERT INTO bears VALUES (5,"Melissa", 8, "F", "dark brown", "nice", 3);
+INSERT INTO bears VALUES (6,"Grinch", 9, "M", "Maroon", "naive", 2);
+INSERT INTO bears VALUES (7,"Wendy", 6, "F", "Black", "calm", 1);
+INSERT INTO bears VALUES (8,null, 5, "M", "Dark brown", "calm", 1);
